85984f981f981d53bfe2d0adf4e96c97c51e3674
scott <scottxxx666@gmail.com>
2017-04-23 06:41:29 +0800

    Install opencart

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart_tw/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart_tw/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart_tw/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart_tw/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/opencart_tw/upload/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/opencart_tw/upload/system/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/opencart_tw/upload/image/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/opencart_tw/upload/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/opencart_tw/upload/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/opencart_tw/upload/system/config/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/opencart_tw/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'web_user');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart_tw/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart_tw/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/opencart_tw/upload/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/opencart_tw/upload/system/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/opencart_tw/upload/image/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/opencart_tw/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/opencart_tw/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/opencart_tw/upload/system/config/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/opencart_tw/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'web_user');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
